1853ab6d48fc4c66386e3734857f2bc6c77b440c
ErmishinD <ermishin.d.e@gmail.com>
2023-08-15 18:19:43 +0300

    bugfix: path to files in public/index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,17 @@
 <?php
 
 // Autoloader
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..//');
 $dotenv->load();
 
 // Config
-require_once '../config.php';
+require_once __DIR__ . '/../config.php';
 
 // Bootstrap
-require_once '../bootstrap.php';
+require_once __DIR__ . '/../bootstrap.php';
 
 // Routes
-require_once '../routes/web.php';
-require_once '../app/Router.php';
+require_once __DIR__ . '/../routes/web.php';
+require_once __DIR__ . '/../app/Router.php';
